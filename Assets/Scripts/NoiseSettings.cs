947f014ac61e491da215cd9d8a110d0c913e808a
Maksymilian Wylaź <maxw.mb78@gmail.com>
2023-12-20 11:05:57 +0100

    Na konsultacje

--- Assets/Scripts/NoiseSettings.cs
+++ Assets/Scripts/NoiseSettings.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [System.Serializable]
 [CreateAssetMenu(fileName = "NoiseSettings", menuName = "Planet Generator/NoiseSettings", order = 0)]
-public class NoiseSettings
+public class NoiseSettings : ScriptableObject 
 {
     public float strength = 1f;
     [Range(1, 8)]
